61d50436bf4d83e0449f7b3207a43f9ca9c9ba42
Dawid <lukasiewicz@onet.com.pl>
2022-11-16 01:02:50 +0100

    trying to paint

--- glwidget.cpp
+++ glwidget.cpp
@@ -3,12 +3,23 @@
 
 GLWidget::GLWidget(QWidget *parent)
 {
+    QSurfaceFormat format;
+    format.setProfile(QSurfaceFormat::CompatibilityProfile);
+    format.setVersion(4, 6);
+    setFormat(format);
 
+    _contextGL = new QOpenGLContext;
+    _contextGL->setFormat(format);
+    _contextGL->create();
+    this->makeCurrent();
+//    _contextGL->makeCurrent(());
+
+    _functionsGL = _contextGL->functions();
 }
 
 void GLWidget::initializeGL()
 {
-    glClearColor(0, 0, 0, 1);
+    glClearColor(1, 0, 0, 1);
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHT0);
     glEnable(GL_LIGHTING);
@@ -18,9 +29,28 @@ void GLWidget::initializeGL()
 void GLWidget::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBegin(GL_QUADS);
+    glVertex2f(-0.5f, -0.5);
+    glVertex2f(-0.5f, 0.5);
+    glVertex2f(0.5f, -0.5);
+    glVertex2f(0.5f, 0.5);
+
+    glEnd();
+    glFlush();
 }
 
 void GLWidget::resizeGL(qint64 w, qint64 h)
 {
 
+}
+
+void GLWidget::reziseEvent(QResizeEvent *event)
+{
+
+}
+
+void GLWidget::paintEvent(QPaintEvent *event)
+{
+    paintGL();
 }
--- glwidget.h
+++ glwidget.h
@@ -4,6 +4,7 @@
 #include <QWidget>
 #include <QOpenGLWidget>
 #include <QSurfaceFormat>
+#include <QOpenGLFunctions>
 #include <QtOpenGL>
 
 class GLWidget: public QOpenGLWidget
@@ -16,6 +17,10 @@ public:
     void resizeGL(qint64 w, qint64 h);
     void reziseEvent(QResizeEvent *event);
     void paintEvent(QPaintEvent *event);
+
+private:
+    QOpenGLContext *_contextGL;
+    QOpenGLFunctions *_functionsGL;
 };
 
 #endif // GLWIDGET_H
